a84f62dd89bf8d0c81883bd7ce1a64df284015b4
Nicolas <nicolaskrusser@gmail.com>
2019-04-21 10:56:24 +0200

    Routing Requests

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-  console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>')
+    res.write('<body>')
+    res.write('<form action="/message" method="POST"><input name="message" type="text"/><button type="submit">Submit</button></form>')
+    res.write('</body>')
+    res.write('</html>')
+    return res.end()
+  } 
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
-  res.write('<head><title>HelloWorld!!</title></head>')
+  res.write('<head><title>Helllo  Node.js</title></head>')
   res.write('<body>')
-  res.write('<h1>Hello World</h1>')
+  res.write('<h1>Helllo from Node.js server</h1>')
   res.write('</body>')
   res.write('</html>')
   res.end()
