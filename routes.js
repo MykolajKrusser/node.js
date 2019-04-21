75f1d63b989c69d7bce2fbc8b99edbed564cf9b4
Nicolas <nicolaskrusser@gmail.com>
2019-04-21 16:59:08 +0200

    Using the Node Modules System

---
+++ routes.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+
+const requestsHandler = (req, res)=>{
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>')
+    res.write('<body>')
+    res.write('<form action="/message" method="POST"><input name="message" type="text"/><button type="submit">Submit</button></form>')
+    res.write('</body>')
+    res.write('</html>')
+    return res.end()
+  } 
+  if (url === '/message' && method === 'POST'){
+    const reqBody = [];
+    req.on('data', (chunk)=>{
+      console.log(chunk);
+      reqBody.push(chunk);
+    });
+    return req.on('end', ()=>{
+      const parseReqBody = Buffer.concat(reqBody).toString();
+      const message = parseReqBody.split('=')[1];
+      fs.writeFile('message.txt', message, (err)=>{
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+    });
+    
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Helllo  Node.js</title></head>')
+  res.write('<body>')
+  res.write('<h1>Helllo from Node.js server</h1>')
+  res.write('</body>')
+  res.write('</html>')
+  res.end()
+}
+
+// module.exports ={
+//   handler: requestsHandler,
+//   hardText: 'Hardcoded text!'
+// } ;
+
+// module.exports.handler = requestsHandler;
+// module.exports.hardText = 'Hardcoded text!';
+
+exports.handler = requestsHandler;
+exports.hardText = 'Hardcoded text!';
