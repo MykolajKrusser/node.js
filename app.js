99350e6e0f74e3384dec72e84e675873b3523fa6
Nicolas <nicolaskrusser@gmail.com>
2019-04-26 18:33:01 +0200

    Adding a 404 Error Page

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shop);
 
-
+app.use((req, res, next)=>{
+  res.status(404).send('<h1>Page Not Found</h1>')
+});
 
 app.listen(3030);
