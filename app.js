43d7919266192e7c2a8b7dae8acef28e38b839be
Nicolas <nicolaskrusser@gmail.com>
2019-04-30 11:50:58 +0200

    Using a Helper Function for File  Navigation

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const rootDir = require('./utils/path');
 
 const app = express();
 
 const adminRoutes = require('./routes/admin');
-const shop = require('./routes/shop')
+const shop = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -13,7 +14,7 @@ app.use('/admin', adminRoutes);
 app.use(shop);
 
 app.use((req, res, next)=>{
-  res.status(404).sendFile(path.join(__dirname, 'views', 'page404.html'))
+  res.status(404).sendFile(path.join(rootDir, 'views', 'page404.html'));
 });
 
 app.listen(3030);
