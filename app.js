e06c439a374c02167793a54b74ecfb94bebaac5e
Nicolas <nicolaskrusser@gmail.com>
2019-05-02 12:19:45 +0200

    Installing & Implementing Pug

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const rootDir = require('./utils/path');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const adminData = require('./routes/admin');
 const shop = require('./routes/shop');
 
